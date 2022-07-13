7aac38406422d3fda57645d0981d3cc1abfafd4a
Dan <drblack651@gmail.com>
2022-07-12 22:55:28 -0500

    Add book page

---
+++ components/Book.jsx
@@ -0,0 +1,14 @@
+export default function Book(props) {
+  const { href, title, blurb, emoji } = props;
+  return (
+    <li>
+      <div className="flex items-center">
+        <span className="inline-flex items-center justify-center h-9 w-9 bg-amber-200 rounded-full">{emoji}</span>
+        <a className="link-style text-2xl ml-2" target="_blank" href={href}>
+          {title}
+        </a>
+      </div>
+      <p className="ml-[3%] pl-4 sm:pl-7 my-1 border-l-2 sm:border-l-4 border-amber-200/75">{blurb}</p>
+    </li>
+  );
+}
