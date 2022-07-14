ec22f81b4088f83cf0cfe7e80298770029e3d2cc
Dan <drblack651@gmail.com>
2022-07-13 19:01:09 -0500

    another one

--- pages/_document.js
+++ pages/_document.js
@@ -4,7 +4,7 @@ export default function Document() {
   return (
     <Html lang='en'>
       <Head />
-      <body className='bg-amber-200/50'>
+      <body className='bg-amber-200'>
         <Main />
         <NextScript />
       </body>
